c8af8d54bf8f1e3ad1c5628c600f52c59c99011a
Drew Neece <dneece5@aggienetwork.com>
2023-02-27 15:31:47 -0600

    add styling to edit form page

--- src/pages/EditCourse/EditCourse.tsx
+++ src/pages/EditCourse/EditCourse.tsx
@@ -28,70 +28,82 @@ const EditCourse = (): JSX.Element => {
   return (
     <form id="edit-form" autoComplete="off" onSubmit={handleSubmit}>
       <h1>Edit Course Info:</h1>
-      <label htmlFor="subject">Subject:</label>
-      <input
-        required
-        type="text"
-        name="subject"
-        id="subject-input"
-        value={form.subject}
-        placeholder="Ex: MATH"
-        onChange={handleChange}
-      />
+      <div id="form">
+        <div>
+          <label htmlFor="subject">Subject: </label>
+          <input
+            required
+            type="text"
+            name="subject"
+            id="subject-input"
+            value={form.subject}
+            placeholder="Ex: MATH"
+            onChange={handleChange}
+          />
+        </div>
 
-      <label htmlFor="code">Code (101-499):</label>
-      <input 
-        required
-        type="number"
-        name="code"
-        id="code-input"
-        value={form.code}
-        placeholder="Ex: 101"
-        onChange={handleChange}
-      />
+        <div>
+          <label htmlFor="code">Code (101-499): </label>
+          <input 
+            required
+            type="number"
+            name="code"
+            id="code-input"
+            value={form.code}
+            placeholder="Ex: 101"
+            onChange={handleChange}
+          />
+        </div>
 
-      <label htmlFor="days">Days:</label>
-      <select 
-        required 
-        name="days" 
-        id="days-select"
-        value={form.days}
-        onChange={handleChange}
-      >
-        <option value="M, W, F">M, W, F</option>
-        <option value="T, Th">T, Th</option>
-      </select>
+        <div>
+          <label htmlFor="days">Days: </label>
+          <select 
+            required 
+            name="days" 
+            id="days-select"
+            value={form.days}
+            onChange={handleChange}
+          >
+            <option value="M, W, F">M, W, F</option>
+            <option value="T, Th">T, Th</option>
+          </select>
+        </div>
 
-      <label htmlFor="time">Time:</label>
-      <select 
-        required 
-        name="time" 
-        id="time-select"
-        value={form.time}
-        onChange={handleChange}
-      >
-        <option value="8:00 - 9:00">8:00 - 9:00</option>
-        <option value="9:20 - 10:20">9:20 - 10:20</option>
-        <option value="10:40 - 11:40">10:40 - 11:40</option>
-        <option value="12:00 - 1:00">12:00 - 1:00</option>
-        <option value="1:20 - 2:20">1:20 - 2:20</option>
-        <option value="2:40 - 3:40">2:40 - 3:40</option>
-        <option value="4:00 - 5:00">4:00 - 5:00</option>
-      </select>
+        <div>
+          <label htmlFor="time">Time: </label>
+          <select 
+            required 
+            name="time" 
+            id="time-select"
+            value={form.time}
+            onChange={handleChange}
+          >
+            <option value="8:00 - 9:00">8:00 - 9:00</option>
+            <option value="9:20 - 10:20">9:20 - 10:20</option>
+            <option value="10:40 - 11:40">10:40 - 11:40</option>
+            <option value="12:00 - 1:00">12:00 - 1:00</option>
+            <option value="1:20 - 2:20">1:20 - 2:20</option>
+            <option value="2:40 - 3:40">2:40 - 3:40</option>
+            <option value="4:00 - 5:00">4:00 - 5:00</option>
+          </select>
+        </div>
 
-      <label htmlFor="hours">Hours:</label>
-      <select 
-        required 
-        name="hours" 
-        id="hours-select"
-        value={form.hours}
-        onChange={handleChange}
-      >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-      </select>
+        <div>
+          <label htmlFor="hours">Hours: </label>
+          <select 
+            required 
+            name="hours" 
+            id="hours-select"
+            value={form.hours}
+            onChange={handleChange}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+        </div>
+      </div>
       <button type="submit">Update Course</button>
     </form>
   )
